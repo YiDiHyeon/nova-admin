d47e2db39cc4d97a5eb86b020b087e53e5860c44
이지현 <leedmswl123@gmail.com>
2025-11-20 23:30:35 +0900

    feat: 로그인 화면 및 기본 폼 검증 추가

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,8 +1,85 @@
+'use client'
+
+import { Field } from '@/components/ui/field'
+import { Input } from '@/components/ui/input'
+import { useForm } from 'react-hook-form'
+import { Button } from '@/components/ui/button'
+import { Card } from '@/components/ui/card'
+import { CircleUserRound, LockKeyhole } from 'lucide-react'
+import { useRouter } from 'next/navigation'
+
+type LoginFormValues = {
+  email: string
+  password: string
+}
 
 export default function Home() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginFormValues>({
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  })
+
+  const router = useRouter()
+  const onSubmit = (values: LoginFormValues) => {
+    console.log('values :', values)
+    router.replace('/dashboard')
+  }
   return (
-    <div>
-        로그인
+    <div className="flex h-screen items-center justify-center">
+      <Card className="w-[480px] px-8 py-6">
+        <div>
+          <h2 className="text-center text-2xl font-extrabold">Nova Admin</h2>
+        </div>
+        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4">
+          <Field>
+            <div className="relative">
+              <CircleUserRound
+                className="absolute top-1/2 left-6 h-4 w-4 -translate-y-1/2"
+                stroke="#cdcdcd"
+              />
+              <Input
+                id="login-id"
+                placeholder="이메일 또는 아이디를 입력해 주세요"
+                className="h-12 rounded-4xl pl-12"
+                {...register('email', {
+                  required: '이메일을 입력해주세요.',
+                })}
+              />
+            </div>
+            {errors.email && <p className="text-xs text-red-500">{errors.email.message}</p>}
+          </Field>
+          <Field>
+            <div className="relative">
+              <LockKeyhole
+                className="absolute top-1/2 left-6 h-4 w-4 -translate-y-1/2"
+                stroke="#cdcdcd"
+              />
+              <Input
+                id="login-password"
+                type="password"
+                placeholder="비밀번호를 입력해 주세요"
+                className="h-12 rounded-4xl pl-12"
+                {...register('password', {
+                  required: '비밀번호를 입력해주세요.',
+                })}
+              />
+            </div>
+            {errors.password && <p className="text-xs text-red-500">{errors.password.message}</p>}
+          </Field>
+          <Button
+            type="submit"
+            className="h-12 w-full rounded-4xl bg-slate-900 text-sm font-medium text-white"
+          >
+            로그인
+          </Button>
+        </form>
+      </Card>
     </div>
-  );
+  )
 }
--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -1,15 +1,17 @@
+// app/(protected)/layout.tsx
 
+import type { ReactNode } from 'react'
+// Sidebar, Header 같은걸 쓸 거면 나중에 여기서 import
 
-export default function Layout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function ProtectedLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="ko">
-      <body>
-        {children}
-      </body>
-    </html>
-  );
+    <div className="flex min-h-screen bg-slate-100">
+      {/* 나중에 Sidebar, Header 추가할 자리 */}
+      {/* <Sidebar /> */}
+      <div className="flex flex-1 flex-col">
+        {/* <Header /> */}
+        <main className="flex-1 p-6">{children}</main>
+      </div>
+    </div>
+  )
 }
